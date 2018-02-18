d11b7dd4c2b04f977ebff99cbc60dead7cf6f94b
Antoine MILLET <amillet@vente-privee.com>
2018-02-18 17:12:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# docker-alpine
+[![License][license-img]][license-href]
+[![pipeline][pipeline-img]][pipeline-href]
+[![docker][docker-img]][docker-href]
+
+## Overview
+
+Alpine Linux  is a security-oriented,  lightweight Linux distribution  based on
+musl libc and busybox.
+
+[alpinelinux.org](https://alpinelinux.org/)
+
+## Description
+
+Use this script to build your own base system.
+
+We've included the last ca-certificates files  in the repository to ensure that
+all of our images are accurates.
+
+## Tags
+
+Supported tags.
+
+- 3.5
+- 3.6
+- 3.7
+
+## Requirements
+
+On Debian, Devuan and Ubuntu you need the following packages:
+
+```bash
+sudo apt-get -qq -y install wget
+```
+
+You also need to be in the docker group to use Docker.
+
+```bash
+sudo usermod -a -G docker ${USER}
+```
+
+Finally you need to login on Docker Hub.
+
+```bash
+docker login
+```
+
+## Usage
+
+You first need to choose which dist between 3.5, 3,6 and 3.7 you want (3.7 will
+be the 'latest' tag) and you need  to choose you user (or organization) name on
+Docker Hub.
+
+Show help.
+
+```bash
+./build.sh -h
+```
+
+Build your own Alpine image (eg. 3.6).
+
+```bash
+./build.sh -d 3.6 -u vpgrp
+```
+
+Build your own Alpine image (eg. 3.7) and push it on the Docker Hub.
+
+```bash
+./build.sh -d 3.7 -u vpgrp -p
+```
+
+## Limitations
+
+Only work on Debian, Devuan and Ubuntu.
+
+## Development
+
+Please read carefully [CONTRIBUTING.md][contribute-href]  before making a merge
+request.
+
+## Miscellaneous
+
+```
+    ╚⊙ ⊙╝
+  ╚═(███)═╝
+ ╚═(███)═╝
+╚═(███)═╝
+ ╚═(███)═╝
+  ╚═(███)═╝
+   ╚═(███)═╝
+```
+
+[license-img]: https://img.shields.io/badge/license-Apache-blue.svg
+[license-href]: /LICENSE
+[pipeline-img]: https://git.vpgrp.io/docker/docker-alpine/badges/master/pipeline.svg
+[pipeline-href]: https://git.vpgrp.io/docker/docker-alpine/commits/master
+[docker-img]: https://img.shields.io/docker/pulls/vpgrp/alpine.svg
+[docker-href]: https://registry.hub.docker.com/u/vpgrp/alpine
+[contribute-href]: /CONTRIBUTING.md
---
+++ build.sh
@@ -0,0 +1,285 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1039
+# shellcheck disable=SC2034
+
+set -e
+
+PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
+arch='amd64'
+version='1.0'
+
+function usage()
+{
+    cat <<EOF
+
+NAME:
+   build.sh - Docker images' builder of Alpine.
+
+USAGE:
+   build.sh -d <dist>
+
+OPTIONS:
+   -h, --help           Show help
+
+   -d, --dist           Choose Alpine distribution
+                        eg: 3.5, 3.6, 3.7, 3.8
+
+   -t, --timezone       Choose your preferred timezone
+                        default: Europe/Amsterdam
+
+   -u, --user           Docker Hub username or organisation
+                        default: $USER
+
+   -p, --push           Docker Hub push
+                        default: no
+
+   -l, --latest         Force the "latest"
+                        default: 3.7
+
+   -v, --verbose        Verbose mode
+
+   -V, --version        Show version
+
+VERSION:
+   docker-alpine version: ${version}
+
+EOF
+}
+
+function docker_bootstrap()
+{
+    # variables
+    cdn="http://dl-cdn.alpinelinux.org"
+    ftp="${cdn}/alpine/v${distid}/releases/x86_64"
+    rootfs="${ftp}/alpine-minirootfs-${distname}-x86_64.tar.gz"
+    image="/tmp/image-${distname}-${arch}"
+
+    echo "-- bootstrap ${distname}" 1>&3
+
+    if [ "$(id -u)" -ne 0 ]
+    then
+        sudo='sudo'
+    fi
+
+    # clean old image
+    if [ -f "${image}.tar" ]
+    then
+        ${sudo} rm -f "${image}.tar"
+    fi
+
+    if [ -f "${image}.tar.gz" ]
+    then
+        ${sudo} rm -f "${image}.tar.gz"
+    fi
+
+    if [ -d "/tmp/image-${distname}-${arch}" ]
+    then
+        ${sudo} rm -fr "${image}"
+    fi
+
+    # download minimal image
+    echo "-- download alpine-minirootfs-${distname}-x86_64.tar.gz" 1>&3
+    ${sudo} curl \
+            --location \
+            --output "${image}.tar.gz" \
+            --silent \
+            "${rootfs}"
+    ${sudo} gunzip "${image}.tar.gz"
+
+    echo "-- extract alpine-minirootfs-${distname}-x86_64.tar.gz" 1>&3
+    ${sudo} mkdir "${image}"
+    ${sudo} tar \
+            -x \
+            -C "${image}" \
+            -f /tmp/alpine.tar.gz \
+            --numeric-owner \
+            -z
+
+    # create /etc/timezone
+    echo ' * /etc/timezone' 1>&3
+    cat <<EOF | \
+        ${sudo} tee "${image}/etc/timezone"
+${timezone}
+EOF
+
+    # create /etc/resolv.conf
+    echo ' * /etc/resolv.conf' 1>&3
+    cat <<EOF | \
+        ${sudo} tee "${image}/etc/resolv.conf"
+nameserver 8.8.4.4
+nameserver 8.8.8.8
+EOF
+
+    # create /etc/apk/repositories
+    echo ' * /etc/apk/repositories' 1>&3
+    cat <<EOF | \
+        ${sudo} tee "${image}/etc/apk/repositories"
+http://mirror.vpgrp.io/alpine/v${distid}/main
+http://mirror.vpgrp.io/alpine/v${distid}/community
+EOF
+
+    # upgrade (without output...)
+    echo ' * apk upgrade' 1>&3
+    # ${sudo} chroot "${image}" sh -c \
+    #         "apk update && \
+    #          apk upgrade &&
+    #          rm -f /var/cache/apk/*"
+
+    # create archive
+    if [ -f "${image}.tar" ]
+    then
+        ${sudo} rm "${image}.tar"
+    fi
+    ${sudo} tar -C "${image}" -c -f "${image}.tar" --numeric-owner .
+
+
+}
+
+# create images from bootstrap archive
+function docker_import()
+{
+    echo "-- docker import from ${image}.tar" 1>&3
+    docker import "${image}.tar" "${user}/alpine:${distname}"
+    docker run "${user}/alpine:${distname}" \
+           echo " * build ${user}/alpine:${distname}" 1>&3
+    docker tag "${user}/alpine:${distname}" "${user}/alpine:${distid}"
+    docker run "${user}/alpine:${distid}" \
+           echo " * build ${user}/alpine:${distid}" 1>&3
+
+    if [ "${distname}" = "${latest}" ]
+    then
+        docker tag "${user}/alpine:${distname}" "${user}/alpine:latest"
+        docker run "${user}/alpine:latest" \
+               echo " * build ${user}/alpine:latest" 1>&3
+    fi
+}
+
+# push image to docker hub
+function docker_push()
+{
+    echo "-- docker push" 1>&3
+    echo " * push ${user}/alpine:${distname}" 1>&3
+    docker push "${user}/alpine:${distname}"
+    echo " * push ${user}/alpine:${distid}" 1>&3
+    docker push "${user}/alpine:${distid}"
+
+    if [ "${distname}" = "${latest}"  ]
+    then
+        echo " * push ${user}/alpine:latest" 1>&3
+        docker push "${user}/alpine:latest"
+    fi
+}
+
+while getopts 'hd:t:u:plvV' OPTIONS
+do
+    case ${OPTIONS} in
+        h)
+            # -h / --help
+            usage
+            exit 0
+            ;;
+        d)
+            # -d / --dist
+            dist=${OPTARG}
+            ;;
+        t)
+            # -t / --timezone
+            timezone=${OPTARG}
+            ;;
+        u)
+            # -u / --user
+            user=${OPTARG}
+            ;;
+        p)
+            # -p / --push
+            push='true'
+            ;;
+        l)
+            # -l / --latest
+            latest=${OPTARG}
+            ;;
+        v)
+            # -v / --verbose
+            verbose='true'
+            ;;
+        V)
+            # -v / --version
+            echo "${version}"
+            exit 0
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ ! -x "$(command -v curl)" ]
+then
+    echo "Please install curl (see README.md)"
+    exit 1
+fi
+
+# -d / --dist
+if [ -n "${dist}" ]
+then
+    case ${dist} in
+        3.5|3.5.2)
+            distname='3.5.2'
+            distid='3.5'
+            ;;
+        3.6|3.6.2)
+            distname='3.6.2'
+            distid='3.6'
+            ;;
+        3.7|3.7.0)
+            distname='3.7.0'
+            distid='3.7'
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+else
+    usage
+    exit 1
+fi
+
+# -t / --timezone
+if [ -z "${timezone}" ]
+then
+    timezone='Europe/Amsterdam'
+fi
+
+# -u / --user
+if [ -z "${user}" ]
+then
+    user=${USER}
+fi
+
+# -l / --latest
+if [ -z "${latest}" ]
+then
+    latest='3.7.0'
+fi
+
+# -v / --verbose
+if [ -z "${verbose}" ]
+then
+    exec 3>&1
+    exec 1>/dev/null
+    exec 2>/dev/null
+else
+    exec 3>&1
+fi
+
+docker_bootstrap
+docker_import
+
+if [ -n "${push}" ]
+then
+    docker_push
+fi
+# EOF
